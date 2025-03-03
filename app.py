880057c354847c166285ac41ceccf5a14d837dc1
sebasbrs <sebasgodoy32@gmail.com>
2025-03-03 15:52:31 -0500

    Fix HTTPS issue

--- app.py
+++ app.py
@@ -56,6 +56,7 @@ def download_video():
 
         file_path = os.path.join(DOWNLOAD_FOLDER, filename)
         stream.download(output_path=DOWNLOAD_FOLDER, filename=filename)
+        app.config['PREFERRED_URL_SCHEME'] = 'https'
         download_url = url_for('get_file', filename=filename, _external=True)
 
         return jsonify({"download_url": download_url})
